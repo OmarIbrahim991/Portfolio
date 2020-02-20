500c79de46bf1a8235692a1452751c13ae77c5a4
OmarIbrahim991 <omar.ibrahim.991@gmail.com>
2020-02-21 00:57:03 +0200

    Updates

---
+++ precache-manifest.9dc240735a6217dc55135f6f96fe3de7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1e0b2fe8ca968a4af0d0b0ca1cd60e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2be360471763ad1f1dfc",
+    "url": "/portfolio/static/css/main.26667a46.chunk.css"
+  },
+  {
+    "revision": "0e31c16e06534dc1dc85",
+    "url": "/portfolio/static/js/2.76584c22.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.76584c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be360471763ad1f1dfc",
+    "url": "/portfolio/static/js/main.e47b7786.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
